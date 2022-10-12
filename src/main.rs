d0ead0d9da54dcc8470f9c33c504a825d692d227
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-10-12 20:19:33 +0200

    Create client in main 🎨

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use hyper::Client;
+use hyper_tls::HttpsConnector;
 mod helpers;
 
 #[tokio::main]
 async fn main() {
-    helpers::get::get("test".to_string()).await;
+    let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+
+    println!("{:?}", helpers::get::get("test".to_string(), client).await);
 }
