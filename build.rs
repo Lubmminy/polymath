65a5b7d7556693ea1a019d8f1ab672d28cf861de
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-10-15 22:58:26 +0200

    Update buid files 🐛

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .compile(
-                &["proto/helloworld/helloworld.proto"],
+                &["proto/crawl/crawl.proto"],
                 &["proto"]
         )
         .unwrap();
