74c2c99344f2f61d8f3eb5b5e1e4c80b21538f2e
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-03-24 21:11:47 +0100

    Delete build.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .compile(
-                &["proto/crawl/crawl.proto"],
-                &["proto"]
-        )
-        .unwrap();
-    Ok(())
-}
