456d3884b705b69ffa3304faf767987e3d58d38f
RealHinome <acn@gravitalia.com>
2024-05-29 12:14:24 +0200

    Support custom errors
    Signed-off-by: RealHinome <acn@gravitalia.com>

--- crates/polymath-error/src/lib.rs
+++ crates/polymath-error/src/lib.rs
@@ -1,14 +1,108 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![forbid(unsafe_code)]
+#![deny(
+    dead_code,
+    unused_imports,
+    unused_mut,
+    missing_docs,
+    missing_debug_implementations
+)]
+//! internal library to provide structures for errors in polymath.
+//!
+//! # Examples
+//! ```rust
+//! use polymath_error::Result;
+//!
+//! fn main() -> Result<()> {
+//!     Ok(())
+//! }
+//! ```
+
+use std::error::Error as StdError;
+use std::fmt;
+
+/// Boxed error to bypass specific [Error](StdError).
+type BError = Box<dyn StdError + Send + Sync>;
+
+/// The struct that represents an error.
+#[derive(Debug)]
+pub struct Error {
+    /// The error type.
+    pub etype: ErrorType,
+    /// The cause of this error.
+    pub cause: Option<BError>,
+    /// Explains the context in which the error occurs.
+    pub context: Option<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Error {
+    /// Throw an [`Error`].
+    pub fn new(
+        etype: ErrorType,
+        cause: Option<BError>,
+        context: Option<String>,
+    ) -> Self {
+        Error {
+            etype,
+            cause,
+            context,
+        }
     }
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.etype)
+    }
+}
+
+impl StdError for Error {}
+
+/// Errors in polymath.
+#[derive(Debug)]
+pub enum ErrorType {
+    /// Generic error that returns no additional information.
+    Unspecified,
+    /// Errors related to `polymath-db`.
+    Database(DatabaseError),
+}
+
+impl fmt::Display for ErrorType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ErrorType::Unspecified => {
+                write!(f, "An error has occurred, but no further information is provided.")
+            },
+            ErrorType::Database(error) => write!(f, "{:?}", error),
+        }
+    }
+}
+impl StdError for ErrorType {}
+
+/// Errors related to `polymath-db`.
+#[derive(Debug)]
+pub enum DatabaseError {
+    /// The connection pool has not been created correctly.
+    PoolCreation,
+    /// The connection pool could not be obtained.
+    PoolObtention,
+    /// The message for the broker has not been sent.
+    MessageNotSent,
+}
+
+impl fmt::Display for DatabaseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DatabaseError::PoolCreation => {
+                write!(f, "The connection pool has not been created correctly.")
+            },
+            DatabaseError::PoolObtention => {
+                write!(f, "The connection pool could not be obtained.")
+            },
+            DatabaseError::MessageNotSent => {
+                write!(f, "The message for the broker has not been sent.")
+            },
+        }
+    }
+}
+
+impl StdError for DatabaseError {}
