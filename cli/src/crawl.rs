99d01863df4fc5e6d7fb09aa6881019a65f5cc6a
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-06-01 16:09:56 +0200

    add crawl command

---
+++ cli/src/crawl.rs
@@ -0,0 +1,51 @@
+//! handle crawler command.
+
+use clap::{
+    error::{ContextKind, ContextValue, ErrorKind},
+    Error,
+};
+use std::{fs, path::PathBuf};
+
+/// Max depth value allowed on cli.
+const MAX_DEPTH: usize = 100;
+
+pub fn handler(
+    url: url::Url,
+    max_depth: usize,
+    _robots_txt: bool,
+    path: Option<PathBuf>,
+) -> Result<(), clap::error::Error> {
+    if max_depth > MAX_DEPTH {
+        let mut err = Error::new(ErrorKind::ValueValidation);
+        err.insert(
+            ContextKind::InvalidArg,
+            ContextValue::String("--max-depth".to_owned()),
+        );
+        err.insert(
+            ContextKind::InvalidValue,
+            ContextValue::Number(max_depth.try_into().unwrap_or_default()),
+        );
+        err.insert(
+            ContextKind::Usage,
+            ContextValue::String(format!("Value cannot exceed {}", MAX_DEPTH)),
+        );
+        err.exit();
+    }
+
+    // Announce what crawler will do.
+    println!(
+        "Crawling {} with a maximum of {} crawled pages.",
+        url, max_depth
+    );
+    
+    if let Some(path) = path {
+        println!(
+            "Saving pages result on {}",
+            path.to_str().unwrap_or_default()
+        );
+
+        fs::create_dir(path)?;
+    }
+
+    Ok(())
+}
