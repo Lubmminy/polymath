602ef2910f55f1fa3bd3ca811204a41e5477d6d4
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-06-15 22:52:57 +0200

    Add test on link extractor

--- crates/polymath-crawler/src/extractor/link.rs
+++ crates/polymath-crawler/src/extractor/link.rs
@@ -13,3 +13,31 @@ pub(crate) fn find_all_links(content: &str) -> Vec<String> {
 
     urls
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn links_extraction() {
+        let html = r#"
+        <body>
+        <p>La vie seule... c'est dur</p>
+        <p>Mais avec notre ami imaginaire, c'est mieux : <a href="https://www.gravitalia.com/">cliquez ici</a></p>
+        <h1>Voir nos actualités : http://news.gravitalia.com/</h1>
+        <script>
+        window.open("https://youtu.be/yG-1v-_1NVM", "_blank");
+        </script>
+        </body>
+        "#;
+
+        assert_eq!(
+            vec![
+                "https://www.gravitalia.com/",
+                "http://news.gravitalia.com/",
+                "https://youtu.be/yG-1v-_1NVM"
+            ],
+            find_all_links(html)
+        );
+    }
+}
